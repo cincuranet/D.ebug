1e1535de9d6400eee0bccf306ac47b36e1b1139d
Jiri Cincura <jiri@x2develop.com>
2019-07-09 13:48:46 +0200

    Initial code.

---
+++ D.ebug/D.ebug.cs
@@ -0,0 +1,13 @@
+﻿using System.Diagnostics;
+
+public static class D
+{
+	[DebuggerStepThrough]
+	public static void ebug()
+	{
+		if (Debugger.IsAttached)
+			Debugger.Break();
+		else
+			Debugger.Launch();
+	}
+}
